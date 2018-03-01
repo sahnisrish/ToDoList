dc6af04e4f172c9842b98a1573dceea2e73309f2
Srishti Sahni <srishti.sahni16@gmail.com>
2018-03-01 23:32:55 +0530

    Tags without Tag selection

---
+++ app/src/main/java/com/example/sahni/todolist/TagView.java
@@ -0,0 +1,14 @@
+package com.example.sahni.todolist;
+
+import android.content.Context;
+import android.widget.TextView;
+
+/**
+ * Created by sahni on 1/3/18.
+ */
+
+public class TagView extends android.support.v7.widget.AppCompatTextView {
+    public TagView(Context context) {
+        super(context);
+    }
+}
